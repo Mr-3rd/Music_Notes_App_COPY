60f5dcefb30532c812171f4fec6ed15d499b0fb8
RageKage <99547837+RageKage@users.noreply.github.com>
2023-11-23 17:50:34 -0600

    added constraint to the note model to not add note to future shows

--- lmn/models.py
+++ lmn/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # Remember that every model gets a primary key field by default.
 
@@ -43,11 +45,19 @@ class Show(models.Model):
 
 class Note(models.Model):
     """ One User's opinion of one Show. """
+    # show = models.ForeignKey(Show, blank=False, on_delete=models.CASCADE, limit_choices_to={'show_date__lt': timezone.now()})
+
     show = models.ForeignKey(Show, blank=False, on_delete=models.CASCADE)
     user = models.ForeignKey('auth.User', blank=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, blank=False)
     text = models.TextField(max_length=1000, blank=False)
     posted_date = models.DateTimeField(auto_now_add=True, blank=False)
+    
+
+    def save(self, *args, **kwargs): # when saving a note, check if the show date is in the future and if yes then raise a validation error
+        if self.show.show_date > timezone.now():
+            raise ValidationError("Cannot add notes to future shows.")
+        super().save(*args, **kwargs) # else save the note
 
     def __str__(self):
         return f'User: {self.user} Show: {self.show} Note title: {self.title} \

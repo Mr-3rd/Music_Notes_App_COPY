3740590f8b785013caa8364d5934bfa72b5bfcba
RageKage <99547837+RageKage@users.noreply.github.com>
2023-11-30 17:09:33 -0600

    unit test for one note per show

--- lmn/tests/test_views.py
+++ lmn/tests/test_views.py
@@ -339,7 +339,7 @@ class TestAddNotesWhenUserLoggedIn(TestCase):
     def test_add_note_database_updated_correctly(self):
         initial_note_count = Note.objects.count()
 
-        new_note_url = reverse('new_note', kwargs={'show_pk': 1})
+        new_note_url = reverse('new_note', kwargs={'show_pk': 3})
 
         response = self.client.post(
             new_note_url, 
@@ -359,7 +359,7 @@ class TestAddNotesWhenUserLoggedIn(TestCase):
         self.assertEqual(now.date(), posted_date.date())  # TODO check time too
 
     def test_redirect_to_note_detail_after_save(self):
-        new_note_url = reverse('new_note', kwargs={'show_pk': 1})
+        new_note_url = reverse('new_note', kwargs={'show_pk': 3})
         response = self.client.post(
             new_note_url, 
             {'text': 'ok', 'title': 'blah blah'}, 
@@ -520,3 +520,15 @@ class TestErrorViews(TestCase):
         # their profiles, or do other activities when it must be verified that the 
         # correct user is signed in (else 403) then this test can be written.
         pass 
+
+class TestOneNotePerShow(TestCase):
+    
+    fixtures = ['testing_users', 'testing_artists', 'testing_shows', 'testing_venues', 'testing_notes']
+
+    def setUp(self):
+        user = User.objects.first()
+        self.client.force_login(user)
+        
+    def test_error_msg_for_more_than_one_note_per_show(self):
+        response = self.client.get(reverse('new_note', kwargs={'show_pk': 1}))
+        self.assertContains(response, 'You can only create one note per show')
